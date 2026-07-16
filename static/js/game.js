072fb8b26f6bdfea2f92d67bd7e3c8d39835ec3d
Gabriel Dinu <gabrieldinu987@gmail.com>
2026-07-16 22:43:22 +0300

    Modificat style.css and game.js

--- static/js/game.js
+++ static/js/game.js
@@ -1,1 +1,29 @@
-console.log("5XO loaded.");
+const BOARD_SIZE = 50;
+
+const board = document.getElementById("board-container");
+
+function createBoard() {
+
+    board.innerHTML = "";
+
+    for (let row = 0; row < BOARD_SIZE; row++) {
+
+        for (let col = 0; col < BOARD_SIZE; col++) {
+
+            const cell = document.createElement("div");
+
+            cell.className = "cell";
+
+            cell.dataset.row = row;
+
+            cell.dataset.col = col;
+
+            board.appendChild(cell);
+
+        }
+
+    }
+
+}
+
+createBoard();
